a60324ea140fe2fed7ec0e14b41f407786b65790
Joy Phoebe <joyapisi@gmail.com>
2023-08-20 15:18:33 +0300

    Add Reservation form slice to fetch data from backend API

---
+++ src/redux/reservations/reservationFormSlice.jsx
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const baseUrl = 'http://localhost:3000/api/v1/houses/1/reservations#new';
+
+const initialState = {
+  reservation_form: {},
+  isLoading: false,
+  error: null,
+};
+
+export const fetchReservationForm = createAsyncThunk(
+  'reservations/fetchReservationForm',
+  async () => {
+    const response = await fetch(baseUrl);
+    const data = await response.json();
+    return data;
+  },
+);
+
+const ReservationFormSlice = createSlice({
+  name: 'reservation_form',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      // .addCase(fetchReservationForm.pending, (state) => {
+      //   state.isLoading = true;
+      // })
+      .addCase(fetchReservationForm.fulfilled, (state, action) => {
+        state.reservation_form = action.payload;
+        state.isLoading = false;
+      });
+    // .addCase(fetchReservationForm.rejected, (state, action) => {
+    //   state.error = action.error.message;
+    //   state.isLoading = false;
+    // });
+  },
+});
+
+export default ReservationFormSlice.reducer;
