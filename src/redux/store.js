94d174c865fc1c06b1f5cd2c178b3f7ba12d94d1
Joy Phoebe <joyapisi@gmail.com>
2023-08-20 15:56:01 +0300

    Add reservation form to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,7 @@ import reservationsListReducer from './reservations/reservationsListSlice';
 const store = configureStore({
   reducer: {
     reservations: reservationsListReducer,
+    reservationForm: reservationFormSlice
   },
 });
 
