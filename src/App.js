859bd40c2dc0498757bdc4c4e1cbeace17beee41
Joy Phoebe <joyapisi@gmail.com>
2023-08-20 11:12:27 +0300

    Add route for reservation form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReservationList from './components/ReservationsList';
+import reservationForm from './components/ReservationForm';
 
 const App = () => (
   <Router>
     <Routes>
-      <Route exact path="/" element={<ReservationList />} />
+      <Route exact path="/reservation-list" element={<ReservationList />} />
+      <Route exact path="/reservation-form" element={<ReservationForm />} />
     </Routes>
   </Router>
 );
