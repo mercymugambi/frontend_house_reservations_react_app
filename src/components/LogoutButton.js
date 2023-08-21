fa32816b4c1dea12ac0e0bc358a4e88ba88e558a
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-21 23:12:33 +0300

    Create a logout and implement the functionality to signout

---
+++ src/components/LogoutButton.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const LogoutButton = () => {
+    const navigate = useNavigate()
+    
+    const NavigateToSplashPage = () => {
+        localStorage.removeItem('userData')
+        navigate('/')
+    }
+
+    return (
+        <button onClick={NavigateToSplashPage}>
+            Logout
+        </button>
+    )
+
+
+
+
+}
+
+export default LogoutButton;
