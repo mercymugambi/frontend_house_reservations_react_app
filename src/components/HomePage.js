b9d6267078b6c2f005e464aea957bd389db04f99
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-21 23:11:40 +0300

    Add Homepage for redirecting after the login

---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'; /* eslint-disable */
+import LogoutButton from './LogoutButton';
+
+const HomePage = () => {
+  const userDataString = localStorage.getItem('userData');
+  const userData = JSON.parse(userDataString);
+
+  return (
+    <div>
+      <h2>Hello {userData.name} Welcome to the Homepage</h2>
+      <LogoutButton />
+    </div>
+  );
+};
+
+export default HomePage;
