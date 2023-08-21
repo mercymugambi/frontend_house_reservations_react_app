0771d0a5ac99a96d107ce5a1c53f303e2102598c
Joy Phoebe <joyapisi@gmail.com>
2023-08-21 14:02:42 +0300

    Update reservations components

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -1,11 +1,25 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+// import DatePicker from 'react-datepicker';
 import { fetchReservationForm } from '../redux/reservations/reservationFormSlice';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const ReservationFormComponent = () => {
+//   const [checkinDate, setCheckinDate] = useState(null);
+//   const [checkoutDate, setCheckoutDate] = useState(null);
+
+  //   const handleCheckinDateChange = (date) => {
+  //     setCheckinDate(date);
+  //     setCheckoutDate(null);
+  //   };
+
+  //   const handleCheckoutDateChange = (date) => {
+  //     setCheckoutDate(date);
+  //   };
+
   const dispatch = useDispatch();
   const { reservationForm, isLoading, error } = useSelector(
-    (state) => state.reservation_form,
+    (state) => state.reservationForm,
   );
 
   useEffect(() => {
@@ -13,8 +27,7 @@ const ReservationFormComponent = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <h2>Reservation Form</h2>
+    <div className="reservation-page">
       {isLoading && <p>Loading...</p>}
       {error && (
       <p>
@@ -24,10 +37,36 @@ const ReservationFormComponent = () => {
       )}
       {reservationForm && (
         <form>
-          {/* Render your form fields here */}
-          <input type="text" placeholder="City" id="city" name="city" value={reservationForm.city} />
-          {/* ... other form fields */}
-          <button type="submit">Submit</button>
+          <h2>Book a House!</h2>
+          <p>
+            Hello! We&apos;re excited that you want to book with us!
+            Please fill in the form to make your reservation.
+          </p>
+          <div className="inputs-and-button">
+            <input
+              type="text"
+              placeholder="City"
+              id="city"
+              name="city"
+              value={reservationForm.city}
+            />
+            {/* <DatePicker
+              id="checkin-date"
+              placeholder="Check-in Date"
+              selected={checkinDate}
+              onChange={handleCheckinDateChange}
+              dateFormat="yyyy-MM-dd"
+            />
+            <DatePicker
+              id="checkout-date"
+              placeholder="Check-out Date"
+              selected={checkoutDate}
+              onChange={handleCheckoutDateChange}
+              minDate={checkinDate ? new Date(checkinDate) : null} // Set minDate for checkout date
+              dateFormat="yyyy-MM-dd"
+            /> */}
+            <button type="submit">Submit</button>
+          </div>
         </form>
       )}
     </div>
--- src/components/ReservationsList.jsx
+++ src/components/ReservationsList.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchReservations } from '../redux/reservations/reservationsListSlice';
+// import ReservationForm from './ReservationForm';
 
-const Reservations = () => {
+const ReservationsList = () => {
   const dispatch = useDispatch();
   const { reservations, isLoading, error } = useSelector(
     (state) => state.reservations,
@@ -13,42 +14,39 @@ const Reservations = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>Reservations</h1>
-      {isLoading && <h2>Message Loading...</h2>}
+    <div className="reservations-page">
+      <h2>Your Resevations</h2>
+      {isLoading && <h2>Loading...</h2>}
       {error && <p>{error}</p>}
-      {reservations && (
       <div className="main-container">
-        {reservations.map((reservation) => (
-          <div key={reservation.id}>
-            <div className="container">
-              <h2>
-                Reservation:
-                {reservation.id}
-              </h2>
-              <p>
-                Check in Date:
-                {reservation.reservation_checkin_date}
-              </p>
-              <p>
-                Checkout Date:
-                {reservation.reservation_checkout_date}
-              </p>
-              <p>
-                User:
-                {reservation.user_id}
-              </p>
-              <p>
-                Reserved House:
-                {reservation.house_id}
-              </p>
-            </div>
+        {reservations && reservations.map((reservation) => (
+          <div className="container" key={reservation.id}>
+            <h4>
+              Reservation:
+              {reservation.id}
+            </h4>
+            <p>
+              Reserved House:
+              {reservation.house_id}
+            </p>
+            <p>
+              City:
+              {reservation.city}
+            </p>
+            <p>
+              Check in Date:
+              {reservation.reservation_checkin_date}
+            </p>
+            <p>
+              Checkout Date:
+              {reservation.reservation_checkout_date}
+            </p>
           </div>
         ))}
       </div>
-      )}
+      {/* <ReservationForm reservation={reservations && reservations[0]} /> */}
     </div>
   );
 };
 
-export default Reservations;
+export default ReservationsList;
