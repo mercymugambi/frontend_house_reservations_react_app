24c9dc9f51ae5922c01d23e7ece44f1206597b20
mumo-esther <mumoesther2021@gmail.com>
2023-08-25 00:43:28 +0300

    Correct fetch issue in housedetails

--- src/components/HouseDetails.js
+++ src/components/HouseDetails.js
@@ -1,71 +1,68 @@
-import React, { useEffect } from "react";
-import { Container } from "react-bootstrap";
-import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchHouses } from "../redux/houses/housesSlice";
 
-function HouseDetails() {
+const HomePage = () => {
   const dispatch = useDispatch();
-
-  const { id } = useParams();
-  console.log("id:", id);
+  const { houses, isLoading, error } = useSelector((state) => state.houses);
 
   useEffect(() => {
-    dispatch(fetchHouses(id));
-  }, [dispatch, id]);
-
-  const houses = useSelector((state) => state.house);
-
-  const filteredHouse = houses.filter((house) => house.show === true);
+    dispatch(fetchHouses());
+  }, [dispatch]);
 
   return (
-    <Container>
-      <div className="HouseDetails">
-        {filteredHouse.map((house) => (
-          <div key={house.id} className="imageContainer">
-            <img src={house.img} alt="house img" className="indiImg" />
-            <div className="cont">
-              <h2>House: {house.name}</h2>
+    <div>
+      {isLoading && <h2>Loading...</h2>}
+      {error && <p>{error}</p>}
+      <div className="main-container">
+        {houses.length > 0 ? (
+          houses.map((house) => (
+            <div className="container">
+              <h4>House: {house.id}</h4>
+              <img src={house.icon} alt="House Icon" />
+              <h2 className="detailTitle">
+                More Information about {house.name}
+              </h2>
+              <div className="info">
+                <p>House Name</p>
+                <p>{house.name}</p>
+              </div>
+              <div className="info">
+                <p>City</p>
+                <p>{house.city || "N/A"}</p>
+              </div>
+              <div className="info">
+                <p>Description</p>
+                <p>{house.description}</p>
+              </div>
+              <div className="info">
+                <p>Bedrooms:</p>
+                <p>{house.bedrooms}</p>
+              </div>
+              <div className="info">
+                <p>Bathrooms</p>
+                <p>{house.bathrooms}</p>
+              </div>
+              <div className="info">
+                <p>Rent:</p>
+                <p>{house.rent}</p>
+              </div>
+              <div className="info">
+                <p>Security Deposit</p>
+                <p>{house.security_deposit}</p>
+              </div>
+              <div className="info">
+                <p>Contact Phone Number</p>
+                <p>{house.contact_phone_number || "N/A"}</p>
+              </div>
             </div>
-
-            <h2 className="detailTitle">More Information about {house.name}</h2>
-            <div className="info">
-              <p>House Name</p>
-              <p>{house.name}</p>
-            </div>
-            <div className="info">
-              <p>City</p>
-              <p>{house.city || "N/A"}</p>
-            </div>
-            <div className="info">
-              <p>Description</p>
-              <p>{house.description}</p>
-            </div>
-            <div className="info">
-              <p>Bedrooms:</p>
-              <p>{house.bedrooms}</p>
-            </div>
-            <div className="info">
-              <p>Bathrooms</p>
-              <p>{house.bathrooms}</p>
-            </div>
-            <div className="info">
-              <p>Rent:</p>
-              <p>{house.rent}</p>
-            </div>
-            <div className="info">
-              <p>Security Deposit</p>
-              <p>{house.security_deposit}</p>
-            </div>
-            <div className="info">
-              <p>Contact Phone Number</p>
-              <p>{house.contact_phone_number || "N/A"}</p>
-            </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>No details available for this house.</p>
+        )}
       </div>
-    </Container>
+    </div>
   );
-}
+};
 
-export default HouseDetails;
+export default HomePage;
