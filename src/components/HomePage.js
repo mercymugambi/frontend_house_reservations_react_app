984cb83565ae8232bfa7621b6b072119a271c7a0
mumo-esther <mumoesther2021@gmail.com>
2023-08-24 23:57:34 +0300

    Add link component to wrap up a house information

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,12 @@
-/* eslint-disable */
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchHouses } from '../redux/houses/housesSlice';
-import LogoutButton from './LogoutButton';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchHouses } from "../redux/houses/housesSlice";
+import LogoutButton from "./LogoutButton";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   const dispatch = useDispatch();
-  const { houses, isLoading, error } = useSelector(
-    (state) => state.houses,
-  );
+  const { houses, isLoading, error } = useSelector((state) => state.houses);
 
   useEffect(() => {
     dispatch(fetchHouses());
@@ -21,31 +19,23 @@ const HomePage = () => {
       {isLoading && <h2>Loading...</h2>}
       {error && <p>{error}</p>}
       <div className="main-container">
-      {houses.length > 0 ? (
-  houses.map((house) => (
-    // {houses && houses.map((house) => (
-          <div className="container" key={house.id}>
-            <h4>
-              House:
-              {house.id}
-            </h4>
-            <img src={house.icon} alt="House Icon" />
-            <p>
-              name:
-              {house.name}
-            </p>
-            <p>
-              description:
-              {house.description}
-            </p>
-          </div>
+        {houses.length > 0 ? (
+          houses.map((house) => (
+            <Link to={`/house/${house.id}`} key={house.id}>
+              <div className="container">
+                <h4>House: {house.id}</h4>
+                <img src={house.icon} alt="House Icon" />
+                <p>name: {house.name}</p>
+                <p>description: {house.description}</p>
+              </div>
+            </Link>
           ))
-          ) : (
-            <p>No houses available.</p>
-          )}
+        ) : (
+          <p>No houses available.</p>
+        )}
       </div>
     </div>
-);
+  );
 };
 
 export default HomePage;
