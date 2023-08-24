ee8c18e132b6b3d757044ad8ef2b4108ca06d108
mumo-esther <mumoesther2021@gmail.com>
2023-08-24 23:56:59 +0300

    Add  details route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Update the import
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import SplashPage from './components/SplashPage';
-import HomePage from './components/HomePage';
-import LoginForm from './components/sessions/LoginForm';
-import RegistrationForm from './components/sessions/RegistrationForm';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Update the import
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import SplashPage from "./components/SplashPage";
+import HomePage from "./components/HomePage";
+import LoginForm from "./components/sessions/LoginForm";
+import RegistrationForm from "./components/sessions/RegistrationForm";
+import HouseDetails from "./components/HouseDetails";
 
 const App = () => (
   <Provider store={store}>
@@ -15,6 +16,7 @@ const App = () => (
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/homepage" element={<HomePage />} />
+        <Route path="/house/:id" element={<HouseDetails />} />
       </Routes>
     </Router>
   </Provider>
