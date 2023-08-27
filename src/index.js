252f797b1e5d13ba8733d87e489cb8f033a2114e
Joy Phoebe <joyapisi@gmail.com>
2023-08-27 16:26:46 +0300

    Delete unrequired files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styling/addHouse.css';
 import './styling/reservations.css';
+import './styling/login.css';
+import './styling/forms.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
