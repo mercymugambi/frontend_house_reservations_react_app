c962bacda7bf3518c5b3a1981b3a6f11f06f09b8
Joy Phoebe <joyapisi@gmail.com>
2023-08-29 23:43:37 +0300

    Add user reducer and ReservationHousesReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import housesReducer from './houses/housesSlice';
 import reservationsListReducer from './reservations/reservationsListSlice';
 import citiesSliceReducer from './reservations/citiesSlice';
+import userReducer from './user/userSlice';
+import reservationHousesReducer from './reservations/reservationHousesSlice';
 
 const store = configureStore({
   reducer: {
+    user: userReducer,
     reservations: reservationsListReducer,
     cities: citiesSliceReducer,
+    reservationHouses: reservationHousesReducer,
     houses: housesReducer,
   },
 });
