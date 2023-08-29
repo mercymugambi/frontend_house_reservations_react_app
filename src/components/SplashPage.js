2076c85045957b269e8203cf097a0e2f66515e4c
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-30 01:29:12 +0300

    Update the splash page with the name of the app

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const SplashPage = () => (
   <div className="splash">
-    <h1>Welcome to Our App!</h1>
+    <h1>Welcome to Classic Rental houses!</h1>
     <p>Join us and explore our amazing features.</p>
     <div className="horizontal-buttons">
       <Link to="/login" className="button">Login</Link>
