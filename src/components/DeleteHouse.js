a72b8e02e29f3e8590eca90fbd055fd59cdf7d9b
Joy Phoebe <joyapisi@gmail.com>
2023-08-31 15:09:06 +0300

    Add successfully deleted message after house deletion

--- src/components/DeleteHouse.js
+++ src/components/DeleteHouse.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteHouse } from '../redux/houses/housesSlice';
 import '../styling/deleteHouse.css';
@@ -6,9 +7,16 @@ import '../styling/deleteHouse.css';
 const DeleteHouse = () => {
   const dispatch = useDispatch();
   const { houses } = useSelector((state) => state.houses);
+  const navigate = useNavigate();
+  const [deletedMessageVisible, setDeletedMessageVisible] = useState(false);
 
   const handleDelete = (id) => {
     dispatch(deleteHouse(id));
+    setDeletedMessageVisible(true);
+    setTimeout(() => {
+      setDeletedMessageVisible(false);
+      navigate('/homepage');
+    }, 2000);
   };
 
   return (
@@ -34,6 +42,11 @@ const DeleteHouse = () => {
           </button>
         </div>
       ))}
+      {deletedMessageVisible && (
+            <div className="deleted-message">
+              <p style={{ color: "red", fontSize: "21px" }}>Successfully deleted!</p>
+            </div>
+          )}
     </div>
   );
 };
