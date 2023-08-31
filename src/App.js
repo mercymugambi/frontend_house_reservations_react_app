dd2b98baf5c386e50c9428917e43e04578eb2d6a
mumo-esther <mumoesther2021@gmail.com>
2023-08-31 09:31:22 +0300

    Update app routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 /* eslint-disable */
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Update the import
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import SplashPage from './components/SplashPage';
-import HomePage from './components/HomePage';
-import LoginForm from './components/sessions/LoginForm';
-import RegistrationForm from './components/sessions/RegistrationForm';
-import HouseForm from './components/addHouse';
-import "./styling/HousesList.css"
-import "./styling/HomePage.css"
-import HouseDetails from './components/HouseDetails';
-import DeleteItem from './components/DeleteItem';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Update the import
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import SplashPage from "./components/SplashPage";
+import HomePage from "./components/HomePage";
+import LoginForm from "./components/sessions/LoginForm";
+import RegistrationForm from "./components/sessions/RegistrationForm";
+import HouseForm from "./components/addHouse";
+import "./styling/HousesList.css";
+import "./styling/HomePage.css";
+import DeleteItem from "./components/DeleteItem";
 
 const App = () => (
-
   <Provider store={store}>
     <Router>
       <Routes>
@@ -24,7 +22,6 @@ const App = () => (
         <Route path="/login" element={<LoginForm />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/add-house" element={<HouseForm />} />
-        <Route path="/house/:id" element={<HouseDetails />} />
         <Route path="/deletehouse" element={<DeleteItem />} />
       </Routes>
     </Router>
